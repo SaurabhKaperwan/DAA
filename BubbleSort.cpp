ffe13cd176fda711f02cb09c26f2435568fc8c5a
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-04-10 19:36:48 +0530

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int a[],int size)
+{
+	int temp;
+	for(int i=0;i<size;i++)
+	{
+		for(int j=i+1;j<size;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+}
+int main()
+{    
+    int a[] = { 10, 35, 32, 13, 26};     
+    int n = sizeof(a)/sizeof(a[0]); 
+    bubbleSort(a,n); 
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<endl;
+}
