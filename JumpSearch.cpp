61bf8100bfb7451274795c64f735d5dc0e95d32c
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-04-10 18:40:56 +0530

    Add files via upload

---
+++ JumpSearch.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int jumpSearch(int a[],int size,int item)
+{
+	int i=0;
+	int j=0;
+	int m=pow(2,0);//block size
+	while(a[m] <= item && m<=size)
+	{
+		i=m;//Starting of block
+		m=pow(2,j);
+		j++;
+	}
+	for(int x=i;x<m;x++)
+	{
+		if(a[x]==item)
+			return x;
+	}
+	return -1;
+}
+int main()
+{
+	int loc=-1;
+	int a[]={1,3,5,6,8};
+	loc=jumpSearch(a,5,8);
+	if(loc>=0)
+		cout<<"Found at:"<<loc+1;
+	else
+		cout<<"Not found";
+
+}
