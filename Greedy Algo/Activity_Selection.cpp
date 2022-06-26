71af0059a71e67480c45c36aaeaf3a2d24d99b65
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2022-06-26 20:31:23 +0530

    Update Activity_Selection.cpp

--- Greedy Algo/Activity_Selection.cpp
+++ Greedy Algo/Activity_Selection.cpp
@@ -1,44 +1,53 @@
-// The following implementation assumes that the activities
-// are already sorted according to their finish time
 #include <bits/stdc++.h>
 using namespace std;
 
-// Prints a maximum set of activities that can be done by a single
-// person, one at a time.
-// n --> Total number of activities
-// s[] --> An array that contains start time of all activities
-// f[] --> An array that contains finish time of all activities
-void printMaxActivities(int s[], int f[], int n)
+// A job has a start time, finish time and profit.
+struct Activitiy
 {
-	int i, j;
+	int start, finish;
+};
 
-	cout <<"Following activities are selected "<< endl;
+// A utility function that is used for sorting
+// activities according to finish time
+bool activityCompare(Activitiy s1, Activitiy s2)
+{
+	return (s1.finish < s2.finish);
+}
+
+// Returns count of the maximum set of activities that can
+// be done by a single person, one at a time.
+void printMaxActivities(Activitiy arr[], int n)
+{
+	// Sort jobs according to finish time
+	sort(arr, arr+n, activityCompare);
+
+	cout << "Following activities are selected n";
 
 	// The first activity always gets selected
-	i = 0;
-	cout <<" "<< i;
+	int i = 0;
+	cout << "(" << arr[i].start << ", " << arr[i].finish << "), ";
 
 	// Consider rest of the activities
-	for (j = 1; j < n; j++)
+	for (int j = 1; j < n; j++)
 	{
 	// If this activity has start time greater than or
 	// equal to the finish time of previously selected
 	// activity, then select it
-	if (s[j] >= f[i])
+	if (arr[j].start >= arr[i].finish)
 	{
-		cout <<" " << j;
+		cout << "(" << arr[j].start << ", "
+			<< arr[j].finish << "), ";
 		i = j;
 	}
 	}
 }
 
-// driver program to test above function
+// Driver program
 int main()
 {
-	int s[] = {1, 3, 0, 5, 8, 5};
-	int f[] = {2, 4, 6, 7, 9, 9};
-	int n = sizeof(s)/sizeof(s[0]);
-	printMaxActivities(s, f, n);
+	Activitiy arr[] = {{5, 9}, {1, 2}, {3, 4}, {0, 6},
+									{5, 7}, {8, 9}};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	printMaxActivities(arr, n);
 	return 0;
 }
-//this code contributed by shivanisinghss2110
